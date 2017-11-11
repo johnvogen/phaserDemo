db23338407f28dc51a79db5e3a7d09217c6f0d5e
John Vogen <john.vogen@verizon.net>
2017-11-10 20:33:51 -0500

    Update

---
+++ phaserDemo/js/statesUnified.js
@@ -0,0 +1,386 @@
+﻿var vtype;
+var vtime;
+var vvalue;
+var timeline = {};
+var head;
+var eyes;
+var body;
+var ben;
+var clipduration;
+
+var bootState = {
+    create: function () {
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.state.start('first');
+    }
+}
+
+var firstState = {
+
+    preload: function () {
+        game.load.spritesheet('heads', 'heads.png', 297, 354, 12);
+        game.load.spritesheet('eyes', 'eyes.png', 106, 128, 11);
+        game.load.image('body', 'body.png');
+        game.load.json('viseme', 'testing.json');
+        game.load.audio('intro', 'testing.mp3');
+    },
+
+    create: function () {
+
+        var text1 = game.add.text(game.world.centerX, game.world.centerY, "first", { font: "16px Arial", fill: "#ff0044", align: "center" });
+
+        //    game.add.sprite(10,20,'image');
+        var prior_vtime = 999;
+        var duration = 0;
+        var count = 0;
+        const default_head = 10;
+        var prior_vframe = default_head;
+        var i = 0;
+
+        // It's visually distracting when the lip frames change too quickly.
+        // set a ms threshold to discard lipsync frames with short durattion
+        const viseme_threshold = 35;
+
+        game.stage.backgroundColor = '#ffffff';
+        heads = game.add.sprite(300, 10, 'heads')
+        eyes = game.add.sprite(405, 95, 'eyes');
+        body = game.add.sprite(280, 353, 'body');
+
+        // Created a sprite grouo called ben.  Working with a group of sprites is easier than working with 
+        // individual sprites for moving and scaling the character
+        ben = game.add.group();
+        ben.add(heads);
+        ben.add(eyes);
+        ben.add(body);
+
+        text1.inputEnabled = true;
+        text1.events.onInputUp.add(up, this);
+
+        // Set a default head postion for initial postion and after lip sync
+        heads.frame = default_head;
+        eyes.frame = 1;
+
+        audiotrack = game.add.audio('intro');
+        timer = game.time.create(false);
+
+        var gameJSON = game.cache.getJSON('viseme');
+        for (var key in gameJSON) {
+            if (gameJSON.hasOwnProperty(key)) {
+                vtype = gameJSON[key].type;
+                vtime = gameJSON[key].time;
+                vvalue = gameJSON[key].value;
+                if (count > 0) {
+                    vframe = getByValue(vframes, vvalue).frame;
+                    // determine display duration
+                    duration = vtime - prior_vtime;
+                    // Only display frames with sufficient duration
+                    if (duration > viseme_threshold) {
+                        for (i = i; i < Math.round(vtime / 10); i++) {
+                            timeline[i] = { "head": prior_vframe };
+                        }
+                        timeline[i] = { "head": vframe };
+                    }
+                    prior_vtime = vtime;
+                    prior_vframe = vframe;
+                }
+            }
+            count++;
+        }
+
+        // Add 1/2 second of default head position.. This helps account for missed ticks and timing differences between
+        // phaser vs. audio file timing 
+        var extension = i + 50;
+        for (i = i + 1; i <= extension; i++) {
+            timeline[i] = { "head": default_head };
+        }
+        clipduration = i;
+
+        // Scale sprite group to 55%
+        ben.scale.setTo(.55, .55);
+        audiotrack.play();
+
+        // Possibly blink eyes every 1/2 second
+        timer.repeat(500, 20000, function () { blink(eyes); }, this);
+        timer.start();
+    },
+
+    update: function () {
+        var tick = Math.round(audiotrack.currentTime / 10);
+        if (tick <= clipduration) {
+            heads.frame = timeline[tick].head;
+        }
+    },
+
+    render: function () {
+        game.debug.text('Time until event: ' + timer.duration.toFixed(0), 32, 32);
+        game.debug.text('Time elapsed: ' + timer.ms.toFixed(0), 32, 64);
+        game.debug.text('Audio mark: ' + audiotrack.currentTime.toFixed(0), 32, 96);
+    }
+
+}
+
+var secondState = {
+
+    preload: function () {
+        game.load.spritesheet('heads', 'heads.png', 297, 354, 12);
+        game.load.spritesheet('eyes', 'eyes.png', 106, 128, 11);
+        game.load.image('body', 'body.png');
+        game.load.json('viseme', 'testing.json');
+        game.load.audio('intro', 'testing.mp3');
+    },
+
+    create: function () {
+
+        var text = game.add.text(game.world.centerX, game.world.centerY, "second", { font: "16px Arial", fill: "#ff0044", align: "center" });
+        text.anchor.set(0.5);
+        text.inputEnabled = true;
+        text.events.onInputUp.add(up, this);
+
+        //    game.add.sprite(10,20,'image');
+        var prior_vtime = 999;
+        var duration = 0;
+        var count = 0;
+        const default_head = 10;
+        var prior_vframe = default_head;
+        var i = 0;
+
+        // It's visually distracting when the lip frames change too quickly.
+        // set a ms threshold to discard lipsync frames with short durattion
+        const viseme_threshold = 35;
+
+        game.stage.backgroundColor = '#ffffff';
+        heads = game.add.sprite(300, 10, 'heads')
+        eyes = game.add.sprite(405, 95, 'eyes');
+        body = game.add.sprite(280, 353, 'body');
+
+        // Created a sprite grouo called ben.  Working with a group of sprites is easier than working with 
+        // individual sprites for moving and scaling the character
+        ben = game.add.group();
+        ben.add(heads);
+        ben.add(eyes);
+        ben.add(body);
+
+        // Set a default head postion for initial postion and after lip sync
+        heads.frame = default_head;
+        eyes.frame = 1;
+
+        audiotrack = game.add.audio('intro');
+        timer = game.time.create(false);
+
+        var gameJSON = game.cache.getJSON('viseme');
+        for (var key in gameJSON) {
+            if (gameJSON.hasOwnProperty(key)) {
+                vtype = gameJSON[key].type;
+                vtime = gameJSON[key].time;
+                vvalue = gameJSON[key].value;
+                if (count > 0) {
+                    vframe = getByValue(vframes, vvalue).frame;
+                    // determine display duration
+                    duration = vtime - prior_vtime;
+                    // Only display frames with sufficient duration
+                    if (duration > viseme_threshold) {
+                        for (i = i; i < Math.round(vtime / 10); i++) {
+                            timeline[i] = { "head": prior_vframe };
+                        }
+                        timeline[i] = { "head": vframe };
+                    }
+                    prior_vtime = vtime;
+                    prior_vframe = vframe;
+                }
+            }
+            count++;
+        }
+
+        // Add 1/2 second of default head position.. This helps account for missed ticks and timing differences between
+        // phaser vs. audio file timing 
+        var extension = i + 50;
+        for (i = i + 1; i <= extension; i++) {
+            timeline[i] = { "head": default_head };
+        }
+        clipduration = i;
+
+        // Scale sprite group to 55%
+        ben.scale.setTo(.55, .55);
+        audiotrack.play();
+
+        // Possibly blink eyes every 1/2 second
+        timer.repeat(500, 20000, function () { blink(eyes); }, this);
+        timer.start();
+    },
+
+    update: function () {
+        var tick = Math.round(audiotrack.currentTime / 10);
+        if (tick <= clipduration) {
+            heads.frame = timeline[tick].head;
+        }
+    },
+
+    render: function () {
+        game.debug.text('Time until event: ' + timer.duration.toFixed(0), 32, 32);
+        game.debug.text('Time elapsed: ' + timer.ms.toFixed(0), 32, 64);
+        game.debug.text('Audio mark: ' + audiotrack.currentTime.toFixed(0), 32, 96);
+    }
+}
+
+var thirdState = {
+
+    preload: function () {
+        game.load.spritesheet('heads', 'heads.png', 297, 354, 12);
+        game.load.spritesheet('eyes', 'eyes.png', 106, 128, 11);
+        game.load.image('body', 'body.png');
+        game.load.json('viseme', 'testing.json');
+        game.load.audio('intro', 'testing.mp3');
+    },
+
+    create: function () {
+
+        var text = game.add.text(game.world.centerX, game.world.centerY, "third", { font: "16px Arial", fill: "#ff0044", align: "center" });
+        text.anchor.set(0.5);
+        text.inputEnabled = true;
+        text.events.onInputUp.add(up, this);
+
+
+        //    game.add.sprite(10,20,'image');
+        var prior_vtime = 999;
+        var duration = 0;
+        var count = 0;
+        const default_head = 10;
+        var prior_vframe = default_head;
+        var i = 0;
+
+        // It's visually distracting when the lip frames change too quickly.
+        // set a ms threshold to discard lipsync frames with short durattion
+        const viseme_threshold = 35;
+
+        game.stage.backgroundColor = '#ffffff';
+        heads = game.add.sprite(300, 10, 'heads')
+        eyes = game.add.sprite(405, 95, 'eyes');
+        body = game.add.sprite(280, 353, 'body');
+
+        // Created a sprite grouo called ben.  Working with a group of sprites is easier than working with 
+        // individual sprites for moving and scaling the character
+        ben = game.add.group();
+        ben.add(heads);
+        ben.add(eyes);
+        ben.add(body);
+
+        // Set a default head postion for initial postion and after lip sync
+        heads.frame = default_head;
+        eyes.frame = 1;
+
+        audiotrack = game.add.audio('intro');
+        timer = game.time.create(false);
+
+        var gameJSON = game.cache.getJSON('viseme');
+        for (var key in gameJSON) {
+            if (gameJSON.hasOwnProperty(key)) {
+                vtype = gameJSON[key].type;
+                vtime = gameJSON[key].time;
+                vvalue = gameJSON[key].value;
+                if (count > 0) {
+                    vframe = getByValue(vframes, vvalue).frame;
+                    // determine display duration
+                    duration = vtime - prior_vtime;
+                    // Only display frames with sufficient duration
+                    if (duration > viseme_threshold) {
+                        for (i = i; i < Math.round(vtime / 10); i++) {
+                            timeline[i] = { "head": prior_vframe };
+                        }
+                        timeline[i] = { "head": vframe };
+                    }
+                    prior_vtime = vtime;
+                    prior_vframe = vframe;
+                }
+            }
+            count++;
+        }
+
+        // Add 1/2 second of default head position.. This helps account for missed ticks and timing differences between
+        // phaser vs. audio file timing 
+        var extension = i + 50;
+        for (i = i + 1; i <= extension; i++) {
+            timeline[i] = { "head": default_head };
+        }
+        clipduration = i;
+
+        // Scale sprite group to 55%
+        ben.scale.setTo(.55, .55);
+        audiotrack.play();
+
+        // Possibly blink eyes every 1/2 second
+        timer.repeat(500, 20000, function () { blink(eyes); }, this);
+        timer.start();
+    },
+
+
+
+    update: function () {
+        var tick = Math.round(audiotrack.currentTime / 10);
+        if (tick <= clipduration) {
+            heads.frame = timeline[tick].head;
+        }
+    },
+
+    render: function () {
+        game.debug.text('Time until event: ' + timer.duration.toFixed(0), 32, 32);
+        game.debug.text('Time elapsed: ' + timer.ms.toFixed(0), 32, 64);
+        game.debug.text('Audio mark: ' + audiotrack.currentTime.toFixed(0), 32, 96);
+    }
+}
+
+var vframes = [
+    { viseme: "p", frame: 9 },
+    { viseme: "t", frame: 6 },
+    { viseme: "T", frame: 11 },
+    { viseme: "f", frame: 8 },
+    { viseme: "k", frame: 6 },
+    { viseme: "i", frame: 4 },
+    { viseme: "r", frame: 6 },
+    { viseme: "u", frame: 11 },
+    { viseme: "s", frame: 6 },
+    { viseme: "S", frame: 0 },
+    { viseme: "@", frame: 5 },
+    { viseme: "a", frame: 4 },
+    { viseme: "o", frame: 1 },
+    { viseme: "O", frame: 2 },
+    { viseme: "E", frame: 6 },
+    { viseme: "e", frame: 5 },
+    { viseme: "sil", frame: 10 },
+];
+
+function up(item) {
+    //item.text = "thanks for clicking!";
+    audiotrack.destroy();
+    if (game.state.current == "first") {
+        game.state.start('second');
+    } else if (game.state.current == "second") {
+        game.state.start('third');
+    } else {
+        game.state.start('first');
+    }
+
+}
+
+function getByValue(arr, value) {
+    for (var i = 0, iLen = arr.length; i < iLen; i++) {
+        if (arr[i].viseme === value) return arr[i];
+    }
+};
+
+function blink(sprite) {
+    // Random number between 0 and 20.
+    var frame = Math.floor(Math.random() * 18);
+    // If frame 0-7, then display that frame.
+    if (frame < 10)
+    { sprite.frame = frame; }
+    // Else, display the default frame. Frame 1.
+    else { sprite.frame = 1; }
+};
+
+var game = new Phaser.Game(800, 500, Phaser.AUTO, 'gameDiv');
+
+game.state.add('boot', bootState);
+game.state.add('first', firstState);
+game.state.add('second', secondState);
+game.state.add('third', thirdState);
+game.state.start('boot');
