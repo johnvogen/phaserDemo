acb810ce2fc2ef08fe9f69c06e30ac67857936ce
John Vogen <john.vogen@Aonhewitt.com>
2017-11-21 15:16:00 -0500

    Add second state

--- phaserDemo/js/base.js
+++ phaserDemo/js/base.js
@@ -18,14 +18,15 @@ var body;
 var ben;
 var clipduration;
 
-
 var text1_1 = "How do you want to pay for your medical expenses?";
-var text1_2 = "Pay a lot less out of my paycheck and more only when I actually need care.";
-var text1_3 = "Pay a lot more out of my paycheck and less when I receive care.";
+var text1_2 = "A. Pay a lot less out of my paycheck and more only when I actually need care.";
+var text1_3 = "B. Pay a lot more out of my paycheck and less when I receive care.";
 
-var text2_1 = "2 How do you want to pay for your benefits?";
-var text2_2 = "2 I'd prefer to pay more when I need care and less in my premium.";
-var text2_3 = "2 I'd prefer to pay less when i need care and more in my premium.";
+var text2_1 = "How often do you think you’ll go to a doctor during the year?";
+var text2_2 = "A. Rarely. 1 or 2 total visits for a minor illness or injury";
+var text2_3 = "B. Occasionally. 3 to 7 total visits for minor illnesses or injuries.";
+var text2_4 = "C. Frequently. 8 or more total visits, or dealing with a serious health condition.";
+var text2_5 = "D. I will only go for my preventive visits.";
 
 var text3_1 = "3 How do you want to pay for your benefits?";
 var text3_2 = "3 I'd prefer to pay more when I need care and less in my premium.";
@@ -58,6 +59,7 @@ var vframes = [
 var gestures = [
     { gesture: "sway", frames: [0, 1, 2, 1, 0] },
     { gesture: "idea", frames: [6, 7, 8, 10, 11, 11, 11, 11, 11, 11, 11, 10, 8, 7, 6] },
+    { gesture: "sway2", frames: [0, 1, 3, 1, 0, 2, 4, 2] },
 ];
 
 var vtype;
@@ -90,18 +92,14 @@ TheGame.firstState.prototype = {
     init: function () {
         //this.scale.scaleMode = Phaser.ScaleManager.RESIZE;
         this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-
     },
-
+ 
     preload: function () {
-        game.load.spritesheet('heads', 'heads.png', 297, 354, 12);
-        game.load.spritesheet('eyes', 'eyes.png', 106, 128, 11);
-        game.load.spritesheet('bodies', 'bodies.png', 803, 832, 21);
-        game.load.json('viseme', 'd81247a6-d59e-4cca-90c6-c2109d13ec7b.json');
-        game.load.audio('intro', 'd81247a6-d59e-4cca-90c6-c2109d13ec7b.mp3');
-
-        //game.load.json('viseme', 'testing.json');
-        //game.load.audio('intro', 'testing.mp3');
+        game.load.spritesheet('heads', 'png/heads.png', 297, 354, 12);
+        game.load.spritesheet('eyes', 'png/eyes.png', 106, 128, 11);
+        game.load.spritesheet('bodies', 'png/bodies.png', 803, 832, 21);
+        game.load.json('viseme', 'data/d81247a6-d59e-4cca-90c6-c2109d13ec7b.json');
+        game.load.audio('intro', 'mp3/d81247a6-d59e-4cca-90c6-c2109d13ec7b.mp3');
     },
 
     create: function () {
@@ -131,8 +129,6 @@ TheGame.firstState.prototype = {
             this.game.canvas.style.cursor = "default";
         }, this);
 
-
-        //    game.add.sprite(10,20,'image');
         var prior_vtime = 999;
         var duration = 0;
         var count = 0;
@@ -230,6 +226,172 @@ TheGame.firstState.prototype = {
     }
 };
 
+TheGame.secondState = function (game) { };
+TheGame.secondState.prototype = {
+    init: function () {
+        //this.scale.scaleMode = Phaser.ScaleManager.RESIZE;
+        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    },
+
+    preload: function () {
+        game.load.spritesheet('heads', 'png/heads.png', 297, 354, 12);
+        game.load.spritesheet('eyes', 'png/eyes.png', 106, 128, 11);
+        game.load.spritesheet('bodies', 'png/bodies.png', 803, 832, 21);
+        game.load.json('viseme', 'data/567956bb-ff6a-4601-bae8-b16e147411ae.json');
+        game.load.audio('intro', 'mp3/567956bb-ff6a-4601-bae8-b16e147411ae.mp3');
+    },
+
+    create: function () {
+        var styleQuestion = { font: "30px Arial", fill: "#000000", wordWrap: true, wordWrapWidth: 600 };
+        var styleAnswer = { font: "26px Arial", fill: "#000000", wordWrap: true, wordWrapWidth: 550 };
+        var xOffset = 50;
+        var xOffset2 = 10;
+        var text1 = game.add.text(game.world.centerX - xOffset, 100, text2_1, styleQuestion);
+        var text2 = game.add.text(game.world.centerX - xOffset2, 220, text2_2, styleAnswer);
+        var text3 = game.add.text(game.world.centerX - xOffset2, 340, text2_3, styleAnswer);
+        var text4 = game.add.text(game.world.centerX - xOffset2, 460, text2_4, styleAnswer);
+        var text5 = game.add.text(game.world.centerX - xOffset2, 580, text2_5, styleAnswer);
+
+        text2.inputEnabled = true;
+        text2.events.onInputUp.add(up, this);
+        text3.inputEnabled = true;
+        text3.events.onInputUp.add(up, this);
+        text4.inputEnabled = true;
+        text4.events.onInputUp.add(up, this);
+        text5.inputEnabled = true;
+        text5.events.onInputUp.add(up, this);
+
+
+
+
+        text2.events.onInputOver.add(function () {
+            this.game.canvas.style.cursor = "pointer";
+        }, this);
+        text2.events.onInputOut.add(function () {
+            this.game.canvas.style.cursor = "default";
+        }, this);
+
+        text3.events.onInputOver.add(function () {
+            this.game.canvas.style.cursor = "pointer";
+        }, this);
+        text3.events.onInputOut.add(function () {
+            this.game.canvas.style.cursor = "default";
+        }, this);
+
+        text4.events.onInputOver.add(function () {
+            this.game.canvas.style.cursor = "pointer";
+        }, this);
+        text4.events.onInputOut.add(function () {
+            this.game.canvas.style.cursor = "default";
+        }, this);
+
+        text5.events.onInputOver.add(function () {
+            this.game.canvas.style.cursor = "pointer";
+        }, this);
+        text5.events.onInputOut.add(function () {
+            this.game.canvas.style.cursor = "default";
+        }, this);
+
+
+
+        var prior_vtime = 999;
+        var duration = 0;
+        var count = 0;
+        const default_head = 10;
+        var prior_vframe = default_head;
+        var i = 0;
+
+        // It's visually distracting when the lip frames change too quickly.
+        // set a ms threshold to discard lipsync frames with short durattion
+        const viseme_threshold = 35;
+
+        game.stage.backgroundColor = stageColor;
+        heads = game.add.sprite(350 + characterOffsetX, 10 + characterOffsetY, 'heads')
+        eyes = game.add.sprite(455 + characterOffsetX, 95 + characterOffsetY, 'eyes');
+        body = game.add.sprite(99 + characterOffsetX, -45 + characterOffsetY, 'bodies');
+
+        // Created a sprite grouo called ben.  Working with a group of sprites is easier than working with 
+        // individual sprites for moving and scaling the character
+        ben = game.add.group();
+        ben.add(heads);
+        ben.add(eyes);
+        ben.add(body);
+
+        // Set a default head postion for initial postion and after lip sync
+        heads.frame = default_head;
+        eyes.frame = 1;
+
+        audiotrack = game.add.audio('intro');
+        timer = game.time.create(false);
+
+        var gameJSON = game.cache.getJSON('viseme');
+        for (var key in gameJSON) {
+            if (gameJSON.hasOwnProperty(key)) {
+                vtype = gameJSON[key].type;
+                vtime = gameJSON[key].time;
+                vvalue = gameJSON[key].value;
+                if (count > 0) {
+                    vframe = getByValue(vframes, vvalue, "viseme").frame;
+                    // determine display duration
+                    duration = vtime - prior_vtime;
+                    // Only display frames with sufficient duration
+                    if (duration > viseme_threshold) {
+                        for (i = i; i < Math.round(vtime / 10); i++) {
+                            timeline[i] = { "head": prior_vframe };
+                        }
+                        timeline[i] = { "head": vframe };
+                    }
+                    prior_vtime = vtime;
+                    prior_vframe = vframe;
+                }
+            }
+            count++;
+        }
+
+        // Add 1/2 second of default head position.. This helps account for missed ticks and timing differences between
+        // phaser vs. audio file timing 
+        var extension = i + 50;
+        for (i = i + 1; i <= extension; i++) {
+            timeline[i] = { "head": default_head };
+        }
+        clipduration = i;
+        // Ends Lipsyncing / clip setup 
+
+        // Start body setup.. default to body 0
+        for (i = 0; i < clipduration; i++) {
+            { timeline[i].body = 0; }
+        }
+        addgesture(getByValue(gestures, "sway2", "gesture"), 500, clipduration, 10);
+
+        // Scale sprite group to 55%
+        ben.scale.setTo(characterScaleX, characterScaleY);
+        console.log(timeline);
+
+        // Start the show
+        audiotrack.play();
+
+        // Possibly blink eyes every 1/2 second
+        timer.repeat(500, 20000, function () { blink(eyes); }, this);
+        timer.start();
+    },
+
+    update: function () {
+        var tick = Math.round(audiotrack.currentTime / 10);
+        if (tick <= clipduration) {
+            heads.frame = timeline[tick].head;
+            body.frame = timeline[tick].body;
+        }
+    },
+
+    render: function () {
+        //game.debug.text('Time until event: ' + timer.duration.toFixed(0), 32, 32);
+        //game.debug.text('Time elapsed: ' + timer.ms.toFixed(0), 32, 64);
+        //game.debug.text('Audio mark: ' + audiotrack.currentTime.toFixed(0), 32, 96);
+        //game.debug.text('AudioTract total duration ' + audiotrack.totalDuration.toFixed(0), 32, 128);
+    }
+
+};
+
 function getByValue(arr, value, mykey) {
     for (var i = 0, iLen = arr.length; i < iLen; i++) {
         if (arr[i][mykey] === value) return arr[i];
@@ -242,18 +404,8 @@ function up(item) {
     if (game.state.current == "first") {//**************************************
         game.state.start('second');
     } else if (game.state.current == "second") {
-        game.state.start('third');
-    } else if (game.state.current == "third") {
-        game.state.start('first');
-        $("#results").css("display", "block");
-    } else if (game.state.current == "wait") {
-        if (mainState === "first") {
-            game.state.start('second');
-        } else if (mainState === "second") {
-            game.state.start('third');
-        } else if (mainState === "third") {
-            game.state.start('first');
-        }
+        alert("Proceed to 3");
+        //game.state.start('third');
     }
 }
 
@@ -281,5 +433,6 @@ window.onload = function () {
     game = new Phaser.Game(1280, 720, Phaser.AUTO, 'gameDiv');
     game.state.add('boot', TheGame.bootState);
     game.state.add('first', TheGame.firstState);
+    game.state.add('second', TheGame.secondState);
     game.state.start('boot');
 };
