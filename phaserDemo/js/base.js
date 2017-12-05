3faa85d35d3daccc1aa3ab8a495ef2af7a7de413
John Vogen <john.vogen@Aonhewitt.com>
2017-12-05 10:41:40 -0500

    tims changes

--- phaserDemo/js/base.js
+++ phaserDemo/js/base.js
@@ -2687,5 +2687,5 @@ window.onload = function () {
     game.state.add('question4', GX.question4State);
     game.state.add('question5', GX.question5State);
     game.state.add('question6', GX.question6State);
-    game.state.start('question1');
+    game.state.start('boot');
 };
